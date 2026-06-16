50d1ff42e0e9678f702458dd7f7f8a4ec06a1bd3
Максим Гафрошка <denaid@MacBook-Air-Maksim.local>
2026-06-16 15:17:53 +0300

    Добавлена модель авторизации

--- access_control/models.py
+++ access_control/models.py
@@ -1,3 +1,78 @@
 from django.db import models
 
-# Create your models here.
+from accounts.models import User
+
+
+class Role(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = "roles"
+
+
+class UserRole(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="user_roles",
+    )
+    role = models.ForeignKey(
+        Role,
+        on_delete=models.CASCADE,
+        related_name="user_roles",
+    )
+
+    def __str__(self):
+        return f"{self.user.email} - {self.role.name}"
+
+    class Meta:
+        db_table = "user_roles"
+        unique_together = ("user", "role")
+
+
+class BusinessElement(models.Model):
+    code = models.CharField(max_length=100, unique=True)
+    name = models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.code
+
+    class Meta:
+        db_table = "business_elements"
+
+
+class AccessRoleRule(models.Model):
+    role = models.ForeignKey(
+        Role,
+        on_delete=models.CASCADE,
+        related_name="access_rules",
+    )
+    element = models.ForeignKey(
+        BusinessElement,
+        on_delete=models.CASCADE,
+        related_name="access_rules",
+    )
+
+    read_permission = models.BooleanField(default=False)
+    read_all_permission = models.BooleanField(default=False)
+
+    create_permission = models.BooleanField(default=False)
+
+    update_permission = models.BooleanField(default=False)
+    update_all_permission = models.BooleanField(default=False)
+
+    delete_permission = models.BooleanField(default=False)
+    delete_all_permission = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.role.name} - {self.element.code}"
+
+    class Meta:
+        db_table = "access_role_rules"
+        unique_together = ("role", "element")
+
+
